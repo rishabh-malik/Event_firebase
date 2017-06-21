b984a74e640d22e36bfc18098190867aac0b367f
aniket sharma <aniket965.as@gmail.com>
2017-06-21 11:40:43 +0530

    Project setup

--- app/src/main/java/com/example/rishabh/event/MainActivity.java
+++ app/src/main/java/com/example/rishabh/event/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rishabh.event;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
